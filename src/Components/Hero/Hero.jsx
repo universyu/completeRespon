ce0faad12c0c4cd638475a524605abfed2b94187
universyu <Lzy500429@gmail.com>
2024-05-27 20:41:51 +0800

    add hero-text

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './hero.css'
-
+import dark_arrow from '../../assets/dark-arrow.png'
 
 const Hero = () => {
     return (
         <div className='hero'>
-
+            <div className="hero-text">
+                <h1>We Ensure better education for a better world</h1>
+                <p>Our cutting-edge curriculum is designed to empower students with the knowledge,skills,and experiences needed to excel in the dynamic field of education</p>
+                <button className={'btn'}>Explore more <img src={dark_arrow} alt=""/> </button>
+            </div>
         </div>
     );
 };
