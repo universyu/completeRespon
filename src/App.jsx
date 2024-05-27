7674e8e2796029ac301ee288a61678d9d054ec1d
universyu <Lzy500429@gmail.com>
2024-05-27 19:48:38 +0800

    add nav and hero

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Hero from "./Components/Hero/Hero.jsx";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './hero.css'
+
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+
+        </div>
+    );
+};
+
+export default Hero;
