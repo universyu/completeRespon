b8a15ecf775a5080e5946da6a4aea78d5eed3ae8
universyu <Lzy500429@gmail.com>
2024-05-28 09:50:06 +0800

    添加Programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
+import Programs from "./Components/Programs/Programs.jsx";
 
 const App = () => {
     return (
         <div>
             <Navbar />
             <Hero />
+            <div className="container">
+                <Programs />
+            </div>
+
         </div>
     );
 };
