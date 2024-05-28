2f9e6644405ae82bf7209fac3ace3d21a24cda24
universyu <Lzy500429@gmail.com>
2024-05-28 23:10:21 +0800

    添加react-scroll

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './Navbar.css'
 import  logo from '../../assets/logo.png'
+import {Link} from "react-scroll";
 
 const Navbar = () => {
 
@@ -16,12 +17,12 @@ const Navbar = () => {
         <nav className={`container ${sticky && 'dark-nav'}`}>
             <img  className="logo" src={logo} alt=""/>
             <ul>
-                <li>Home</li>
-                <li>Program</li>
-                <li>About us</li>
-                <li>Campus</li>
-                <li>Testimonials</li>
-                <li><button className='btn'>Contack us</button></li>
+                <li><Link to={'hero'} smooth={true} offset={-200} duration={500}>Home</Link></li>
+                <li><Link to={'programs'} smooth={true} offset={-200} duration={500}>Program</Link></li>
+                <li><Link to={'about'} smooth={true} offset={-100} duration={500}>About us</Link></li>
+                <li><Link to={'campus'} smooth={true} offset={-200} duration={500}>Campus</Link></li>
+                <li><Link to={'testimonials'} smooth={true} offset={-200} duration={500}>Testimonials</Link></li>
+                <li><Link to={'contact'} smooth={true} offset={0} duration={500} className={'btn'}>Contact us</Link></li>
             </ul>
         </nav>
     );
