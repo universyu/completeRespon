d9f77403f62ce6562e5b3f046ae52bed77315259
universyu <Lzy500429@gmail.com>
2024-05-29 11:09:40 +0800

    添加video

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Hero from "./Components/Hero/Hero.jsx";
 import Programs from "./Components/Programs/Programs.jsx";
@@ -8,8 +8,13 @@ import Campus from "./Components/Campus/Campus.jsx";
 import Testimonials from "./Components/Testimonials/Testimonials.jsx";
 import Contact from "./Components/Contact/Contact.jsx";
 import Fooder from "./Components/Fooder/Fooder.jsx";
+import VideoPlayer from "./Components/VideoPlayer/VideoPlayer.jsx";
 
 const App = () => {
+
+    const [playState,setPlayState] = useState(false);
+
+
     return (
         <div>
             <Navbar />
@@ -17,7 +22,7 @@ const App = () => {
             <div className="container">
                 <Title subTitle={'Our PROGRAM'} title={'What We Offer'} />
                 <Programs />
-                <About />
+                <About setPlayState={setPlayState}/>
                 <Title subTitle={'Gallery'} title={'Campus Photos'}></Title>
                 <Campus />
                 <Title subTitle={'TESTIMONIALS'} title={'What Student Says'}></Title>
@@ -26,7 +31,7 @@ const App = () => {
                 <Contact />
                 <Fooder />
             </div>
-
+            <VideoPlayer playState={playState} setPlayState={setPlayState}/>
         </div>
     );
 };
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,12 +4,12 @@ import about_img from '../../assets/about.png'
 import play_icon from '../../assets/play-icon.png'
 
 
-const About = () => {
+const About = ({setPlayState}) => {
     return (
         <div className={'about'}>
             <div className="about-left">
                 <img src={about_img} className={'about-img'} alt=""/>
-                <img src={play_icon} className={'play-icon'} alt=""/>
+                <img src={play_icon} className={'play-icon'} alt="" onClick={() => {setPlayState(true)}}/>
             </div>
             <div className="about-right">
                 <h3>ABOUT UNIVERSITY</h3>
