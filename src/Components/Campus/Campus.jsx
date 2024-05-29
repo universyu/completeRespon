6f02ce59388a830c45127482f9d5557fc92c94e0
universyu <Lzy500429@gmail.com>
2024-05-29 13:27:29 +0800

    优化界面，去掉无效按钮

--- src/Components/Campus/Campus.jsx
+++ src/Components/Campus/Campus.jsx
@@ -4,7 +4,6 @@ import gallery_1 from '../../assets/gallery-1.png'
 import gallery_2 from '../../assets/gallery-2.png'
 import gallery_3 from '../../assets/gallery-3.png'
 import gallery_4 from '../../assets/gallery-4.png'
-import white_arrow from '../../assets/white-arrow.png'
 
 const Campus = () => {
     return (
@@ -15,7 +14,6 @@ const Campus = () => {
                 <img src={gallery_3} alt=""/>
                 <img src={gallery_4} alt=""/>
             </div>
-            <button className={'btn dark-btn'}>See more here <img src={white_arrow} alt=""/></button>
         </div>
     );
 };
